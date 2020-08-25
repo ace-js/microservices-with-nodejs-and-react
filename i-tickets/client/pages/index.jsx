aeabacca87ab81c867e08a12184305c5897c7425
Arnaud Bertieaux <bertieaux.a@gmail.com>
2020-08-25 19:17:59 +0200

    Add conditional welcome message

--- i-tickets/client/pages/index.jsx
+++ i-tickets/client/pages/index.jsx
@@ -1,11 +1,16 @@
 import buildClient from "../api/build-client";
 
-const LandingPage = ({ currentUser }) => (
-  <div className="container">
-    <h1>Landing Page</h1>
-    {currentUser && <p>You are logged in as "{currentUser.email}"</p>}
-  </div>
-);
+const LandingPage = ({ currentUser }) => {
+  const welcomeText = currentUser
+    ? `You are logged in as "${currentUser.email}"`
+    : "You are not logged in";
+  return (
+    <div className="container">
+      <h1>Landing Page</h1>
+      <p>{welcomeText}</p>
+    </div>
+  );
+};
 
 // fetch initial data during server side rendering process and not in the component with didMount lifecycle
 LandingPage.getInitialProps = async (context) => {
